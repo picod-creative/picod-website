b7e0110e105f9b0dbb9df0843e4c18d917e20870
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2022-03-13 16:17:44 +0300

    update navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+import { FC } from 'react';
+import {
+  AppBar,
+  Button,
+  Hidden,
+  IconButton,
+  Link as MuiLink,
+  Stack,
+  useTheme,
+} from '@mui/material';
+import PicodIcon from '../icons/Picod';
+import Link from 'next/link';
+import MenuIcon from '../icons/Menu';
 
-const Navbar: React.FC = () => {
-  return <header></header>;
+const Navbar: FC = () => {
+  const theme = useTheme();
+
+  return (
+    <AppBar
+      elevation={0}
+      color="transparent"
+      position="fixed"
+      sx={{
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        py: 3.5,
+        px: 4.5,
+        [theme.breakpoints.down('md')]: {
+          py: 1,
+          px: 0,
+        },
+      }}
+    >
+      <Link href="/" passHref>
+        <MuiLink sx={{ display: 'flex' }}>
+          <PicodIcon variant="icon" />
+        </MuiLink>
+      </Link>
+      <Hidden mdUp implementation="css">
+        <IconButton sx={{ mr: 2, p: 2 }}>
+          <MenuIcon />
+        </IconButton>
+      </Hidden>
+      <Hidden mdDown implementation="css">
+        <Stack direction="row" alignItems="center" spacing={4} sx={{ pr: 4 }}>
+          <Link href="/about" passHref>
+            <MuiLink underline="none" color="inherit">
+              A propos
+            </MuiLink>
+          </Link>
+          <Link href="/contact" passHref>
+            <Button
+              variant="contained"
+              color="inherit"
+              sx={{
+                borderRadius: 100,
+                textTransform: 'none',
+                color: '#666666',
+              }}
+              component={MuiLink}
+            >
+              Contact
+            </Button>
+          </Link>
+        </Stack>
+      </Hidden>
+    </AppBar>
+  );
 };
 
 export default Navbar;
