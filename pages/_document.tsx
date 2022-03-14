9cb8af63b8724eaf70b6b79a91b627758a7db490
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2022-03-14 10:50:23 +0300

    include roboto & public sans fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,14 @@
-import * as React from "react";
+import * as React from 'react';
 import Document, {
   DocumentInitialProps,
   Html,
   Head,
   Main,
   NextScript,
-} from "next/document";
-import createEmotionCache from "../src/createEmotionCache";
-import createEmotionServer from "@emotion/server/create-instance";
-import theme from "../src/theme";
+} from 'next/document';
+import createEmotionCache from '../src/createEmotionCache';
+import createEmotionServer from '@emotion/server/create-instance';
+import theme from '../src/theme';
 
 interface MyDocumentProps extends DocumentInitialProps {
   emotionStyleTags: JSX.Element[];
@@ -22,9 +22,15 @@ export default class MyDocument extends Document<MyDocumentProps> {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {this.props.emotionStyleTags}
@@ -84,7 +90,7 @@ MyDocument.getInitialProps = async (ctx) => {
   const emotionStyles = extractCriticalToChunks(initialProps.html);
   const emotionStyleTags = emotionStyles.styles.map((style) => (
     <style
-      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
       key={style.key}
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: style.css }}
