58114f141d201c0ac98cff6cd5007e5f5030056c
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2022-04-07 13:42:54 +0300

    update meta viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
-import "../styles/globals.css";
-import type { AppPropsWithLayout } from "../src/@types";
-import type { EmotionCache } from "@emotion/cache";
-import createEmotionCache from "../src/createEmotionCache";
-import { CacheProvider } from "@emotion/react";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "../src/theme";
-import { CssBaseline } from "@mui/material";
+import '../styles/globals.css';
+import type { AppPropsWithLayout } from '../src/@types';
+import type { EmotionCache } from '@emotion/cache';
+import createEmotionCache from '../src/createEmotionCache';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../src/theme';
+import { CssBaseline } from '@mui/material';
+import Head from 'next/head';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -22,12 +23,20 @@ function MyApp({
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {getLayout(<Component {...pageProps} />)}
-      </ThemeProvider>
-    </CacheProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        />
+      </Head>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {getLayout(<Component {...pageProps} />)}
+        </ThemeProvider>
+      </CacheProvider>
+    </>
   );
 }
 
