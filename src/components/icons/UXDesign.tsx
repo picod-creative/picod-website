505994ab79ce6fef9b17b8de6ee1663a792a2b91
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2022-04-18 17:30:58 +0300

    add new ui/ux design icons

---
+++ src/components/icons/UXDesign.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+import { SvgIcon, SvgIconProps } from '@mui/material';
+
+const UXDesign: FC<SvgIconProps> = (props) => (
+  <SvgIcon
+    {...props}
+    viewBox="0 0 34 34"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M17.4678 19.914V34H31.3987V19.914H17.4678ZM0.0541992 34H13.9851V19.914H0.0541992V34ZM0.0541992 2.30658V16.3925H13.9851V2.30658H0.0541992ZM23.8412 0L13.9851 9.94821L23.8412 19.914L33.6973 9.94821L23.8412 0Z"
+      fill="currentColor"
+    />
+  </SvgIcon>
+);
+
+export default UXDesign;
