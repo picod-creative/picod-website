28f2a924feee0ce5db83b9edda48c1535e69533f
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2022-06-28 17:55:04 +0300

    use InView component to update SectionNav state

--- src/components/partials/index/Section.tsx
+++ src/components/partials/index/Section.tsx
@@ -2,10 +2,10 @@ import {
   createRef,
   forwardRef,
   HTMLAttributes,
-  useEffect,
   useImperativeHandle,
 } from 'react';
 import { Container, ContainerProps, Typography } from '@mui/material';
+import InView from '../../common/InView';
 
 export type SectionProps = HTMLAttributes<HTMLElement> &
   ContainerProps<'section'> & {
@@ -21,83 +21,68 @@ const Section = forwardRef<HTMLElement, SectionProps>(
 
     useImperativeHandle(ref, () => sectionRef.current!);
 
-    useEffect(() => {
-      const onScroll: (this: Document, event: Event) => void = function (e) {
-        if (sectionRef.current) {
-          const { y, height } = sectionRef.current.getBoundingClientRect();
-          const scrollThreshold = 50;
-
-          if (!active && y <= scrollThreshold && y > scrollThreshold - height) {
-            onEnter?.();
-          }
-        }
-      };
-
-      document.addEventListener('scroll', onScroll);
-
-      return () => document.removeEventListener('scroll', onScroll);
-    }, [active, onEnter, sectionRef]);
-
     return (
-      <Container
-        ref={sectionRef}
-        component="section"
-        {...props}
-        sx={(theme) => ({
-          [theme.breakpoints.down('md')]: {
-            py: 5,
-          },
-          ...(typeof (props.sx as any) === 'function'
-            ? (props.sx as any)(theme)
-            : props.sx),
-          py: 6.5,
-        })}
-      >
-        <Typography
-          variant="h1"
+      <InView targetRef={sectionRef} onEnter={onEnter} threshold={160}>
+        <Container
+          ref={sectionRef}
+          component="section"
+          {...props}
           sx={(theme) => ({
-            position: 'relative',
-            fontSize: theme.typography.pxToRem(36),
-            textTransform: 'uppercase',
-            fontWeight: 800,
-            lineHeight: 1,
-            mb: 6.25,
-            '&::after': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              right: 0,
-              bottom: theme.spacing(2.25),
-              backgroundColor: theme.palette.secondary.main,
-              width: 330,
-              height: 2,
-            },
             [theme.breakpoints.down('md')]: {
-              mb: 1.5,
-              fontSize: theme.typography.pxToRem(24),
-              '&::after': {
-                width: '21vw',
-                bottom: theme.spacing(0.5),
-              },
+              py: 5,
             },
+            ...(typeof (props.sx as any) === 'function'
+              ? (props.sx as any)(theme)
+              : props.sx),
+            py: 6.5,
           })}
         >
-          {prefix ? (
-            <>
-              <Typography
-                component="span"
-                color="secondary"
-                sx={{ textTransform: 'none', fontWeight: 500, lineHeight: 1 }}
-              >
-                {prefix}
-              </Typography>
-              <br />
-            </>
-          ) : null}
-          {title}
-        </Typography>
-        {children}
-      </Container>
+          <Typography
+            variant="h1"
+            sx={(theme) => ({
+              position: 'relative',
+              fontSize: theme.typography.pxToRem(36),
+              textTransform: 'uppercase',
+              fontWeight: 800,
+              lineHeight: 1,
+              mb: 6.25,
+              '&::after': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                right: 0,
+                bottom: theme.spacing(2.25),
+                backgroundColor: theme.palette.secondary.main,
+                width: 330,
+                height: 2,
+              },
+              [theme.breakpoints.down('md')]: {
+                mb: 1.5,
+                fontSize: theme.typography.pxToRem(24),
+                '&::after': {
+                  width: '21vw',
+                  bottom: theme.spacing(0.5),
+                },
+              },
+            })}
+          >
+            {prefix ? (
+              <>
+                <Typography
+                  component="span"
+                  color="secondary"
+                  sx={{ textTransform: 'none', fontWeight: 500, lineHeight: 1 }}
+                >
+                  {prefix}
+                </Typography>
+                <br />
+              </>
+            ) : null}
+            {title}
+          </Typography>
+          {children}
+        </Container>
+      </InView>
     );
   }
 );
