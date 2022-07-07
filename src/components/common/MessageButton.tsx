7590e4df7c518c5fbe48bb53805527c4bc3dbfde
Hajanirina Ridjvan RANDRIANANDRAINA <darijavan@gmail.com>
2022-07-07 15:56:01 +0300

    refactoring

--- src/components/common/MessageButton.tsx
+++ src/components/common/MessageButton.tsx
@@ -1,37 +1,33 @@
-import { FC } from 'react';
-import { IconButton, IconButtonProps, useTheme } from '@mui/material';
+import { FC, memo } from 'react';
+import { IconButton, IconButtonProps } from '@mui/material';
 import MessageIcon from '../icons/Message';
 
-const MessageButton: FC<IconButtonProps> = (props) => {
-  const theme = useTheme();
-
-  return (
-    <IconButton
-      {...props}
-      sx={{
-        width: 'fit-content',
-        position: 'fixed',
-        aspectRatio: '1',
-        p: 2,
-        zIndex: (theme) => theme.zIndex.tooltip,
-        bottom: (theme) => theme.spacing(6),
-        right: (theme) => theme.spacing(6),
+const MessageButton: FC<IconButtonProps> = (props) => (
+  <IconButton
+    {...props}
+    sx={(theme) => ({
+      width: 'fit-content',
+      position: 'fixed',
+      aspectRatio: '1',
+      p: 2,
+      zIndex: (theme) => theme.zIndex.tooltip,
+      bottom: (theme) => theme.spacing(6),
+      right: (theme) => theme.spacing(6),
+      [theme.breakpoints.down('md')]: {
+        bottom: (theme) => theme.spacing(3),
+        right: (theme) => theme.spacing(3),
+      },
+    })}
+  >
+    <MessageIcon
+      sx={(theme) => ({
+        fontSize: '3rem',
         [theme.breakpoints.down('md')]: {
-          bottom: (theme) => theme.spacing(3),
-          right: (theme) => theme.spacing(3),
+          fontSize: '2.5rem',
         },
-      }}
-    >
-      <MessageIcon
-        sx={{
-          fontSize: '3rem',
-          [theme.breakpoints.down('md')]: {
-            fontSize: '2.5rem',
-          },
-        }}
-      />
-    </IconButton>
-  );
-};
+      })}
+    />
+  </IconButton>
+);
 
-export default MessageButton;
+export default memo(MessageButton);
