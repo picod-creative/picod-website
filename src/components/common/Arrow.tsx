37f18d365e9da5c23a2a076b1f60aa4702729038
Hajanirina Ridjvan RANDRIANANDRAINA <darijavan@gmail.com>
2022-07-16 12:43:44 +0300

    use responsive value for thickness & size props

--- src/components/common/Arrow.tsx
+++ src/components/common/Arrow.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
-import { Box, SxProps, Theme, useTheme } from '@mui/material';
+import { Box, SxProps, Theme } from '@mui/material';
+import { ResponsiveStyleValue } from '@mui/system';
+import useResponsiveStyleValue from '../../hooks/useResponsiveStyleValue';
 
 export interface ArrowProps {
   direction: 'up' | 'down' | 'left' | 'right';
-  size?: number;
-  thickness?: number;
+  size?: ResponsiveStyleValue<number>;
+  thickness?: ResponsiveStyleValue<number>;
   color?: string;
   sx?: SxProps<Theme>;
 }
@@ -12,70 +14,85 @@ export interface ArrowProps {
 const Arrow: FC<ArrowProps> = ({
   direction,
   color,
-  thickness = 4,
-  size = 80,
+  thickness,
+  size,
   sx = {},
 }) => {
-  const theme = useTheme();
-  color = color ?? theme.palette.primary.main;
+  const thicknessValue = useResponsiveStyleValue(thickness) ?? 4;
+  const sizeValue = useResponsiveStyleValue(size) ?? 80;
 
   return (
     <Box
-      sx={(theme) => ({
-        height: direction === 'up' || direction === 'down' ? size : thickness,
-        width: direction === 'up' || direction === 'down' ? thickness : size,
-        py:
-          direction === 'up' || direction === 'down' ? 0 : `${thickness / 2}px`,
-        px:
-          direction === 'up' || direction === 'down' ? `${thickness / 2}px` : 0,
-        display: 'flex',
-        boxSizing: 'content-box',
-        flexDirection:
-          direction === 'up' || direction === 'down' ? 'column' : 'row',
-        alignItems: 'center',
-        ...(typeof sx === 'function' ? (sx as any)(theme) : sx),
-        '&:before, &:after': {
-          content: '""',
-          display: 'block',
-        },
-        '&:before': {
-          flexGrow: 1,
+      sx={(theme) => {
+        color = color ?? theme.palette.primary.main;
+
+        return {
           height:
-            direction === 'right' || direction === 'left' ? '60%' : undefined,
-          width: direction === 'up' || direction === 'down' ? '60%' : undefined,
-          backgroundColor: color,
-        },
-        '&:after': {
-          width: 0,
-          height: 0,
-          order: direction === 'up' || direction === 'left' ? -1 : undefined,
-          borderTop:
-            direction === 'down'
-              ? `${thickness}px solid ${color}`
-              : direction === 'right' || direction === 'left'
-              ? `${thickness}px solid transparent`
-              : undefined,
-          borderLeft:
-            direction === 'right'
-              ? `${thickness}px solid ${color}`
-              : direction === 'up' || direction === 'down'
-              ? `${thickness}px solid transparent`
-              : undefined,
-          borderRight:
-            direction === 'left'
-              ? `${thickness}px solid ${color}`
-              : direction === 'up' || direction === 'down'
-              ? `${thickness}px solid transparent`
-              : undefined,
-          borderBottom:
-            direction === 'up'
-              ? `${thickness}px solid ${color}`
-              : direction === 'right' || direction === 'left'
-              ? `${thickness}px solid transparent`
-              : undefined,
-          zIndex: -1,
-        },
-      })}
+            direction === 'up' || direction === 'down'
+              ? sizeValue
+              : thicknessValue,
+          width:
+            direction === 'up' || direction === 'down'
+              ? thicknessValue
+              : sizeValue,
+          py:
+            direction === 'up' || direction === 'down'
+              ? 0
+              : `${thicknessValue / 2}px`,
+          px:
+            direction === 'up' || direction === 'down'
+              ? `${thicknessValue / 2}px`
+              : 0,
+          display: 'flex',
+          boxSizing: 'content-box',
+          flexDirection:
+            direction === 'up' || direction === 'down' ? 'column' : 'row',
+          alignItems: 'center',
+          ...(typeof sx === 'function' ? (sx as any)(theme) : sx),
+          '&:before, &:after': {
+            content: '""',
+            display: 'block',
+          },
+          '&:before': {
+            flexGrow: 1,
+            height:
+              direction === 'right' || direction === 'left' ? '60%' : undefined,
+            width:
+              direction === 'up' || direction === 'down' ? '60%' : undefined,
+            backgroundColor: color,
+          },
+          '&:after': {
+            width: 0,
+            height: 0,
+            order: direction === 'up' || direction === 'left' ? -1 : undefined,
+            borderTop:
+              direction === 'down'
+                ? `${thickness}px solid ${color}`
+                : direction === 'right' || direction === 'left'
+                ? `${thickness}px solid transparent`
+                : undefined,
+            borderLeft:
+              direction === 'right'
+                ? `${thickness}px solid ${color}`
+                : direction === 'up' || direction === 'down'
+                ? `${thickness}px solid transparent`
+                : undefined,
+            borderRight:
+              direction === 'left'
+                ? `${thickness}px solid ${color}`
+                : direction === 'up' || direction === 'down'
+                ? `${thickness}px solid transparent`
+                : undefined,
+            borderBottom:
+              direction === 'up'
+                ? `${thickness}px solid ${color}`
+                : direction === 'right' || direction === 'left'
+                ? `${thickness}px solid transparent`
+                : undefined,
+            zIndex: -1,
+          },
+        };
+      }}
     />
   );
 };
